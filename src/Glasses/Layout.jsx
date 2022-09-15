fc1b0baf40cb626082df3fd6fc2d5df3f6d76527
Truong <nguyenvantruong9876>
2022-09-15 22:06:28 +0700

    hoan thien

--- src/Glasses/Layout.jsx
+++ src/Glasses/Layout.jsx
@@ -3,45 +3,53 @@ import dataGlasses from '../data/dataGlasses.json'
 
 export default class Layout extends Component {
     state = {
-        glasses: dataGlasses
+        glasses:  {
+            "id": 3,
+            "price": 30,
+            "name": "DIOR D6700HQ",
+            "url": "./glassesImage/v3.png",
+            "desc": "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. "
+        }     
     }
     renderlist = () => {
-        return dataGlasses.map((Glasses, i) => { 
-            return <img onClick={()=> {this.changeGlasses(Glasses)}} src={Glasses.url} alt="" className="col-2" style={{ width: '110px', cursor: 'pointer',  backgroundColor:'white', width:'180px', padding:'18px'}} key={i}  />
-         })
+        return dataGlasses.map((Glasses, i) => {
+            return <img onClick={() => { this.changeGlasses(Glasses) }} src={Glasses.url} alt="" className="col-2" style={{ width: '110px', cursor: 'pointer', backgroundColor: 'white', width: '180px', padding: '18px' }} key={i} />
+        })
     }
     changeGlasses = (newGla) => {
         this.setState({
-            glasses:newGla
-            
+            glasses: newGla
+
         })
     }
 
     render() {
         return (
-            <div style={{ backgroundImage: "url(./glassesImage/background.jpg)", backgroundSize: "cover", height: "100%"}}>
-                <div style={{backgroundColor: "#00000033", height: "100vh"}}>
+            <div style={{ backgroundImage: "url(./glassesImage/background.jpg)", backgroundSize: "cover", height: "100%" }}>
+                <div style={{ backgroundColor: "#00000033", height: "100vh" }}>
                     <h3 style={{ color: "#f0f8ff" }} className="text-center text-light p-5">
                         TRY GLASSES APP ONLINE
                     </h3>
-                    <div className='container'>
+                    <div className='container-fluid'>
                         <div className='row text-center'>
-                            <div className="col-3 " >
-                                <img src="./glassesImage/model.jpg" alt="model.jpg" />
-                               
+
+                            <div className="col-3 left-model  model-actyvity" >
+                                <img  src="./glassesImage/model.jpg" alt="model.jpg" />
+
                             </div>
-                            <div className="col-3 " >
+
+                            <div className="col-3 right-model model-actyvity">
                                 <div >
-                                    <img  src="./glassesImage/model.jpg" alt="model.jpg" />
+                                    <img src="./glassesImage/model.jpg" alt="model.jpg" />
                                 </div>
-                              
-                                <img src={this.state.glasses.url} alt="" />
-                                <div >
+                                <img className='glass'  src={this.state.glasses.url} alt="" />
+                                <div className="model-info">
                                     
-                                    <span  style={{background: '#ce781c'}}>{this.state.glasses.name}</span><br/> <span style={{background: '#ce781c'}}>{this.state.glasses.price}</span><br/>
-                                    <span  style={{background: '#ce781c'}}>{this.state.glasses.desc}</span>
+                                    <span className='model-name' style={{ background: '#ce781c' }}>{this.state.glasses.name}</span><br /> <span style={{ background: '#ce781c' }}>{this.state.glasses.price}</span><br />
+                                    <span className='modle-des' style={{ background: '#ce781c' }}>{this.state.glasses.desc}</span>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                     <div className="bg-light container text-center mt-5 d-flex justify-content-center p-5">
